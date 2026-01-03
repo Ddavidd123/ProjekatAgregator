f4420a9473ad4c7cf86c25ece4786f2839d11947
Ddavidd123 <stosicdavid6@gmail.com>
2026-01-04 00:52:17 +0100

    dodavanje strukture Node

---
+++ Agregator/Node.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <vector>
+using namespace std;
+
+enum class OperationMode {
+	AUTOMATIC,
+	BATCH
+};
+
+class Node {
+private:
+	int nodeId;
+	Node* parent;
+	vector<Node*> children;
+	double aggregatedConsumption;
+	OperationMode mode;
+
+	//za BATCH rezim
+	vector<double> pendingConsumptions;
+
+public:
+	Node(int id, OperationMode m = OperationMode::AUTOMATIC);
+
+	void addChild(Node* child);
+	void setParent(Node* p);
+	void receiveConsumption(double value);
+	void aggregate();
+	void sendToParent();
+	void requestConsumption();
+	void setMode(OperationMode m);
+
+	int getId() const;
+	double getAggregatedConsumption() const;
+
+	//Za BATCH rezim
+	void processBatch();
+
+
+
+};
